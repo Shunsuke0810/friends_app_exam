8df873a4b434c08108878811be35058c43bdfecb
SHUNSUKE <shunsuke0810@gmail.com>
2022-06-16 11:10:19 +0900

    空行、インデントレベルの修正を行いました

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -48,7 +48,6 @@ class PicturesController < ApplicationController
         end
       end
     end
-
   end
 
   # PATCH/PUT /pictures/1 or /pictures/1.json
@@ -75,13 +74,12 @@ class PicturesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def picture_params
-      params.require(:picture).permit(:content, :image, :image_cache, :user_id)
-    end
+  def set_picture
+    @picture = Picture.find(params[:id])
+  end
+
+  def picture_params
+    params.require(:picture).permit(:content, :image, :image_cache, :user_id)
+  end
 end
